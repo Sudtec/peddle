c113de638c728b00c5325984ba04e165514060a2
Sodiq <oyekunlesodiq7@gmail.com>
2024-04-20 19:47:59 +0100

    wip

--- server.ts
+++ server.ts
@@ -3,15 +3,28 @@ import cors from "cors";
 const productRouter = require("./routes/productRoutes");
 
 const app = express();
+// ** CORS **
+let whitelist: string[] = [
+  "http://localhost:3000",
+  "https://peddle.vercel.app",
+];
+if (process.env.NODE_ENV !== "production") {
+  whitelist = [...whitelist, "http://localhost:3000"];
+}
 
+const corsOptions = {
+  origin: (origin: any, callback: any) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback("Not allowed by CORS", false);
+    }
+  },
+};
+
+app.use(cors(corsOptions));
 app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
 
 app.use("/products", productRouter);
 
